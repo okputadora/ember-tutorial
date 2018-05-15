2f831eaff528ffa8bbc954763c224d62a6e58576
okputadora <okputadora@gmail.com>
2018-05-15 12:18:57 -0400

    withhold filtered results if query has changed by the time results are fetched

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -13,7 +13,11 @@ export default Component.extend({
     handleFilterEntry() {
       let filterInputValue = this.get('value');
       let filterAction = this.get('filter');
-      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+      filterAction(filterInputValue).then(filterResults => {
+        if (filterResults.query === this.get('value')) {
+          this.set('results', filterResults.results);
+        }
+      });
     }
   }
 
